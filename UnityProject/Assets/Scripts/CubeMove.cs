113c17a417d49e9d301ac4954f0f676aeb1dfa8b
liuhao_121804 <liuhao0794@163.com>
2020-08-20 17:50:19 +0800

    unity的测试webgl工程提交

---
+++ UnityProject/Assets/Scripts/CubeMove.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeMove : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public int moveSpeed = 5;
+    public int speed = 200;
+    public float y = 1;
+    public float x = 1;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.back * moveSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.left * moveSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * moveSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetMouseButton(0))
+        {//鼠标按着左键移动 
+            y = Input.GetAxis("Mouse X") * Time.deltaTime * speed;
+            x = Input.GetAxis("Mouse Y") * Time.deltaTime * speed;
+        }
+        else
+        {
+            x = y = 0;
+        }
+        transform.Rotate(new Vector3(0, y, 0));
+    }
+
+    
+}
